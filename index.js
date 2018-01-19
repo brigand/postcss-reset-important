2cdf0408de2710037c31b28d9d3c1c5d8d6c9638
Frankie Bagnardi <f.bagnardi@gmail.com>
2018-01-19 15:09:50 -0700

    basically working

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
-var postcss = require('postcss');
+const postcss = require('postcss');
+
+function addReset(props, rule, noImportant, before = null) {
+  Object.keys(props).reverse().map((key) => {
+    const obj = { prop: key, value: props[key], important: !noImportant };
+    if (before) {
+      rule.insertBefore(before, obj);
+    } else {
+      rule.prepend(obj);
+    }
+  });
+}
 
 module.exports = postcss.plugin('postcss-reset-important', function (opts) {
-    opts = opts || {};
+  opts = opts || {};
 
-    // Work with options here
+  const requireExplicit = !!opts.requireExplicit;
+  const noImportant = !!opts.noImportant;
+  const props = opts.props || {
+    color: 'inherit',
+    margin: '0',
+    padding: '0',
+    border: '0',
+    'font-size': '100%',
+    font: 'inherit',
+    'vertical-align': 'baseline',
+  };
 
-    return function (root, result) {
+  let doReset = !requireExplicit;
 
-        // Transform CSS AST here
+  const transform = (css) => {
+    css.walkRules((rule) => {
+      let didReset = false;
+      let doReset = !requireExplicit;
+      const unsetProps = Object.assign({}, props);
+      rule.walkDecls((decl, i) => {
+        // reset may be true or false
+        if (decl.prop === 'reset') {
+          if (decl.value === 'false') {
+            doReset = false;
+          }
+          if (decl.value === 'true') {
+            doReset = true;
+          }
+          if (doReset) {
+            // Check the rest of the props so we can omit some resets
+            rule.walkDecls((decl2) => {
+              if (unsetProps[decl2.prop]) {
+                delete unsetProps[decl2.prop];
+              }
+            });
 
-    };
+            // Add the reset and store that we did it so it doesn't get added twice
+            if (!didReset) {
+              addReset(unsetProps, rule, noImportant, decl);
+              didReset = true;
+            }
+          }
+          decl.remove();
+        }
+
+        if (!noImportant) {
+          decl.important = true;
+        }
+
+        if (unsetProps[decl.prop]) {
+          delete unsetProps[decl.prop];
+        }
+      });
+
+      if (doReset && !didReset) addReset(unsetProps, rule, noImportant, null);
+    });
+  };
+
+  return transform;
 });
+
--- index.test.js
+++ index.test.js
@@ -3,17 +3,31 @@ var postcss = require('postcss');
 var plugin = require('./');
 
 function run(input, output, opts) {
-    return postcss([ plugin(opts) ]).process(input)
-        .then(result => {
-            expect(result.css).toEqual(output);
-            expect(result.warnings().length).toBe(0);
-        });
+  return postcss([ plugin(opts) ]).process(input, { from: undefined })
+    .then(result => {
+      expect(result.warnings().length).toBe(0);
+      if (output) {
+        expect(result.css).toEqual(output);
+      } else {
+        expect(result.css).toMatchSnapshot();
+      }
+      return result.css;
+    });
 }
 
-/* Write tests here
-
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+it('does basic reset', async () => {
+  const css = await run('a{ }', null, { });
+  expect(css).toMatch(/inherit/);
+  expect(css).toMatch(/!important/);
 });
 
-*/
+it('no reset with reset: false', async () => {
+  const css = await run('a{ reset: false }', null, { });
+  expect(css).not.toMatch(/inherit/);
+});
+
+it('adds important to own classes', async () => {
+  const css = await run('a{ color: red; }', null, { });
+  expect(css).toMatch(/color: red !important/);
+});
+
